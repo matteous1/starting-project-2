68aaa82804a2e3cbb0e6fa5e66f551f8d16dd0a9
Matteo Schiatti <matteo.schiatti@abstract-technology.com>
2022-11-14 16:43:39 +0100

    grid and navigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,12 @@
 import { View, Pressable, Text, StyleSheet, Platform } from 'react-native'
 
-const CategoryGridTile = ({ title, color }) => {
+const CategoryGridTile = ({ title, color, onPress }) => {
   return (
     <View style={styles.gridItem}>
       <Pressable
         android_ripple={{ color: '#ccc' }}
         style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
+        onPress={onPress}
       >
         <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,13 +3,20 @@ import CategoryGridTile from '../components/CategoryGridTile'
 
 import { CATEGORIES } from '../data/dummy-data'
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
 
   const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id,
+      });
+    }
+
     return (
       <CategoryGridTile
         title={itemData.item.title}
         color={itemData.item.color}
+        onPress={pressHandler}
       />
     )
   } 
